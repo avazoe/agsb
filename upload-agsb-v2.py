becbca1de078ede3154abeddc2ebe270cf10f287
avazoe <w1026xp@gmail.com>
2025-06-06 10:34:13 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "avazoe"         # 用户名
+UUID = "bb6de853-1057-4c89-8689-d3512b85cdc4"                     # UUID，留空则自动生成
+PORT = 49999                # Vmess端口，留空或0则自动生成
+DOMAIN = "avazoe.top"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiNjdhYTM0N2ZkZmUwNDA0NDMzZWQ1OWYyMzAxYmY1ZTkiLCJ0IjoiYWRiNDMxYjYtOWMyNy00OTlkLTlhZWQtMjcwMGQxZTNkM2JhIiwicyI6Ik1USm1OVGhoWldFdE1qZzJNQzAwTXpOaExXSTROell0WkRGbE16RmtOMk5qT1RjeCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
